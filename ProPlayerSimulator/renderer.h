3f27f9d5cc379fd5e8df91117e5d7775e9102158
fewizz <hazeevaidar@gmail.com>
2018-07-21 19:51:18 +0300

    Похоже нужно перечитать желе супербиблию =_=

--- ProPlayerSimulator/renderer.h
+++ ProPlayerSimulator/renderer.h
@@ -1,8 +1,14 @@
 #pragma once
 #include "gl.h"
+#include "glm/vec2.hpp"
+#include "glm/mat4x4.hpp"
 #include <vector>
 
 //class renderer_combined;
+class matrix_provider {
+	virtual glm::mat4x4 combined() = 0;
+	glm::mat4x4 operator()() { return combined(); }
+};
 
 class renderer {
 public:
@@ -13,12 +19,12 @@ public:
 };
 //////////////////////////////////////////
 
-class renderer_combinable:public renderer {
+/*class renderer_combinable:public renderer {
 public:
 	virtual bool combinable(renderer other) { return false; };
-};
+};*/
 //////////////////////////////////////////
-class renderer_combined : public renderer_combinable {
+class renderer_combined : public virtual renderer {
 	std::vector<renderer&> vec;
 public:
 	renderer_combined() {}
@@ -40,12 +46,41 @@ public:
 	}*/
 };
 /////////////////////////////////////////
-class renderer_with_program: public renderer {
+/*class renderer_with_program: public virtual renderer {
 	const gl::program& p;
 public:
 	renderer_with_program(const gl::program& pr) :p{ pr } {}
-};
+};*/
 /////////////////////////////////////////
-class quad_renderer :public renderer_with_program {
+class quad_renderer :public virtual renderer {
+public:
+	quad_renderer(matrix_provider mp, glm::vec2 p1, glm::vec2 p2) {
+
+	}
+
+	void render() override {
+		static gl::program program{
+			gl::vertex_shader(R"(
+#version 430 core
+
+layout (location = 0) uniform mat4;
+layout (location = 1) uniform vec2 v[4];
+
+void main() {
+	gl_Position = mat * vec4(v[gl_VertexID]);
+}
+			)"),
+			gl::fragment_shader(R"(
+#version 430 core
+
+out vec4 c;
+void main() {
+	c = vec4(1.);
+}
+			)")
+		};
+
+
+	}
 
 };
